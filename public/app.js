7b7a22f7129e40142371de01ae88f883dde88da0
David Rawson <dm.rawson@gmail.com>
2018-02-20 18:05:38 +0000

    Initial commit. Lots to do.

---
+++ public/app.js
@@ -0,0 +1,38 @@
+const initialize = function(){
+
+  const scunnyButton = document.querySelector('#scunnyButton')
+  scunnyButton.addEventListener('click', handleButtonClick)
+  // button.addEventListener('click', handleButtonClick);
+
+
+  const center   = {lat: 54.116332, lng: -0.082774};
+  const oldLighthouse = {lat: 54.117777, lng: -0.089587};
+  const mapDiv = document.getElementById('main-map');
+
+  const mainMap = new MapWrapper( center, mapDiv, 16);
+  mainMap.addMarker(center);
+  mainMap.addInfoWindow(center, newLighthouseString)
+  // mainMap.addMarker(oldLighthouse);
+  mainMap.addInfoWindow(oldLighthouse, oldLighthouseString)
+  mainMap.addClickEvent();
+}
+
+const handleButtonClick = function(){
+  console.log("handleButton click");
+  mainMap.goToScunny()
+};
+
+
+const newLighthouseString = "The present lighthouse, designed by Samuel Wyatt \nand costing £8,000 to build, was first lit \non 1 December 1806. The current electric fog \nsignal was installed in 1975, \nreplacing older equipment."
+const oldLighthouseString = "The first lighthouse, built by Sir John Clayton, \nwas completed in 1674 and is one of \nthe oldest surviving complete lighthouse \nin England. Built from chalk, it was never lit."
+document.addEventListener("DOMContentLoaded", initialize)
+
+
+//scunny
+// 53.581066, -0.660172
+
+//new
+// 54.116332, -0.082774
+
+//old
+// 54.117777, -0.089587
---
+++ public/mapWrapper.js
@@ -0,0 +1,45 @@
+const MapWrapper = function(coords, container, zoom){
+  this.googleMap = new google.maps.Map(container, {
+    // center: {lat: 40.712784, lng: -74.005941},
+    center: coords,
+    zoom: zoom
+  })
+}
+
+MapWrapper.prototype.addInfoWindow = function (coords, contentString) {
+  const window = new google.maps.InfoWindow({
+    position: coords,
+    map: this.googleMap,
+    content: contentString,
+  })
+};
+
+MapWrapper.prototype.goToScunny = function () {
+  this.googleMap.setCenter({lat: 53.581066, lng: -0.660172})
+};
+
+MapWrapper.prototype.addMarker = function (coords) {
+  // assigning the marker to a variable isn't necessary
+  // but may be useful at a later time.
+  const marker = new google.maps.Marker({
+    position: coords,
+    map: this.googleMap
+  });
+};
+
+MapWrapper.prototype.addClickEvent = function () {
+  google.maps.event.addListener(this.googleMap, 'click', function(event){
+    console.log(event);
+    console.log(event.latLng.lat());
+
+    // You don't construct a string. IT IS AN OBJECT !
+    let coords = {lat:event.latLng.lat(), lng:event.latLng.lng()};
+    this.addMarker(coords);
+  }.bind(this))
+};
+
+// {lat: 56.620201, lng: -3.943670}
+// 56.620201, -3.943670
+
+
+// setCenter{lat: 53.581066, lng: -0.660172}
